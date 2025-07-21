129968fe8687653258a05d60a565abad2f4ca130
Harsh Rajput <harsh@Harshs-MacBook-Pro.local>
2025-07-21 10:52:36 +0530

    add fetch repo list api call using axios

--- src/api/github.js
+++ src/api/github.js
@@ -1,28 +1,32 @@
+import axios from 'axios';
+
 const GITHUB_API_URL = 'https://api.github.com/orgs/godaddy/repos';
 
-const mockRepos = [
-  {
-    id: 1,
-    name: "repo-one",
-    description: "desc-one",
-    html_url: "https://www.google.com",
-    language: "JS",
-    forks_count: 5,
-    open_issues_count: 2,
-    watchers_count: 10,
-  },
-  {
-    id: 2,
-    name: "repo-two",
-    description: "desc-two",
-    html_url: "https://www.yahoo.com",
-    language: "TS",
-    forks_count: 3,
-    open_issues_count: 1,
-    watchers_count: 7,
-  },
-];
+// const mockRepos = [
+//   {
+//     id: 1,
+//     name: "repo-one",
+//     description: "desc-one",
+//     html_url: "https://www.google.com",
+//     language: "JS",
+//     forks_count: 5,
+//     open_issues_count: 2,
+//     watchers_count: 10,
+//   },
+//   {
+//     id: 2,
+//     name: "repo-two",
+//     description: "desc-two",
+//     html_url: "https://www.yahoo.com",
+//     language: "TS",
+//     forks_count: 3,
+//     open_issues_count: 1,
+//     watchers_count: 7,
+//   },
+// ];
 
 export const fetchAllRepos = async () => {
-  return mockRepos;
+  //return mockRepos;
+  const response = await axios.get(GITHUB_API_URL);
+  return response.data;
 };
