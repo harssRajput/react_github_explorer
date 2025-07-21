0f3c544fc258e17f1f37df9c505efd9a239cca02
Harsh Rajput <harsh@Harshs-MacBook-Pro.local>
2025-07-21 10:41:30 +0530

    add loading, error and side-effect api call logic for Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RepoList from '../components/RepoList';
-
-const mockRepos = [
-  {
-    id: 1,
-    name: "repo-one",
-    description: "desc-one",
-    html_url: "https://www.google.com",
-    language: "JS",
-    forks_count: 5,
-    open_issues_count: 2,
-    watchers_count: 10,
-  },
-  {
-    id: 2,
-    name: "repo-two",
-    description: "desc-two",
-    html_url: "https://www.yahoo.com",
-    language: "TS",
-    forks_count: 3,
-    open_issues_count: 1,
-    watchers_count: 7,
-  },
-];
+import {fetchAllRepos} from '../api/github';
 
 const Home = () => {
-  const [repos, _] = useState(mockRepos);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const loadRepos = async () => {
+      try {
+        const fetchAllReposResponse = await fetchAllRepos();
+        if (!fetchAllReposResponse || fetchAllReposResponse.length === 0) {
+          throw new Error('No repositories found');
+        }
+        setRepos(fetchAllReposResponse);
+      } catch (err) {
+        console.error("[Home] error calling api", err);
+        setError(`Failed to load respose ${err.status} ${err.message}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadRepos();
+  }, []);
+
+  if (loading) return <p className="loading">Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <div className="home-container">
       <h1>GoDaddy Github Repositories</h1>
